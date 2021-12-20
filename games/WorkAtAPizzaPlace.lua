e9e500bc56031b156e46db3fb09bb92c1eeebd4c
technorav3nn <colin01460@outlook.com>
2021-12-20 12:22:13 -0800

    sdf

--- games/WorkAtAPizzaPlace.lua
+++ games/WorkAtAPizzaPlace.lua
@@ -8,14 +8,6 @@ local client = players.LocalPlayer
 
 local playerChar = client.Character or client.Character:Wait()
 
-local function FindFirstCustomer()
-    for _, customer in pairs(workspace.Customers:GetChildren()) do
-        if customer:FindFirstChild("Head") and customer:FindFirstChild("Humanoid") and customer.Head.CFrame.Z < 100 then
-            return c
-        end
-    end
-end
-
 local flags = {}
 function flags:SetFlag(name, value)
     flags[name] = value
@@ -32,6 +24,71 @@ local function getHousePart(address)
             return h.Upgrades:GetChildren()[1].GivePizza
         end
     end
+end
+local players = game:GetService("Players")
+local client = players.LocalPlayer
+local playerChar = client.Character or client.Character:Wait()
+
+local houses = game:GetService("Workspace").Houses
+
+local boxes = {}
+
+local function tpBack()
+    task.wait(1)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(63, 7, -9)
+end
+
+local function getPizzaBox(box)
+    if box:IsA("Tool") and box:FindFirstChild("House") then
+        local boxCf = box.Handle.CFrame
+        playerChar.HumanoidRootPart.CFrame = boxCf + Vector3.new(0, 3, 0)
+        task.wait(0.3)
+    end
+end
+
+local function populatePizzaBoxes()
+    for _, v in pairs(client.Backpack:GetChildren()) do
+        if v:FindFirstChild("House") then
+            table.insert(boxes, v)
+        end
+    end
+    for _, v in pairs(playerChar:GetChildren()) do
+        if v:FindFirstChild("House") then
+            table.insert(boxes, v)
+        end
+    end
+end
+
+local function deliverPizzaToHouse(address)
+    for _, v in pairs(houses:GetChildren()) do
+        if v.Address.Value == address then
+            local housePart = v.Upgrades:GetChildren()[1]:FindFirstChild("GivePizza")
+            if housePart then
+                local tool = client.Backpack:FindFirstChild(address)
+                if tool then
+                    playerChar.Humanoid:EquipTool(tool)
+                end
+                task.wait()
+                playerChar.HumanoidRootPart.CFrame = housePart.CFrame
+                task.wait(5)
+            end
+        end
+    end
+end
+
+for _, v in pairs(game.Workspace:GetChildren()) do
+    if v:FindFirstChild("House") then
+        getPizzaBox(v)
+    end
+end
+
+populatePizzaBoxes()
+if #boxes == 0 then
+    tpBack()
+end
+
+for _, pizza in ipairs(boxes) do
+    deliverPizzaToHouse(pizza.Name)
 end
 
 Section:NewToggle(
